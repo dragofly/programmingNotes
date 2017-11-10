aefcb94d3152cfadc19c6c9cbe82bd55dca539b9
Juraj <juraj.murgic@gmail.com>
2017-11-10 07:26:17 +0100

    JS express notes added
    - node modues added to ignore

--- 70_480_HTML _JS_CSS/jsfiddle.js
+++ 70_480_HTML _JS_CSS/jsfiddle.js
@@ -1,10 +1,25 @@
-//require
-var dice = require('./dice')
-console.log(dice.dice);
+// //require
+// var dice = require('./dice')
+// console.log(dice.dice);
 
-console.error("You sed:" + process.argv[2]);
-console.warn(typeof console.log == "function");
+// console.error("You sed:" + process.argv[2]);
+// console.warn(typeof console.log == "function");
 
+var gradeBook = require("./nodeUntitTests/lib/gradeBook.js").gradeBook;
+var express = require("express");
+var app = express();
 
-//automated testing
-//node unit
+app.get("/", function(req, res) {
+    res.send("Hi user, wellcome");
+});
+
+// url:/grade?grades=1,2,3,4
+app.get("/grade", function(req, res) {
+    let grades = req.query.grades.split(",");
+    for (let i = 0; i < grades.length; i++) {
+        gradeBook.addGrade(parseInt(grades[i]));
+    }
+    res.send("Number of grades is:" + gradeBook.getGradeCount());
+});
+
+app.listen(3000);
